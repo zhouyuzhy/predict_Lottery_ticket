d0f5a7177dab648c5c193db7a7823fbd8ae73387
xishou <xishou@xiaohongshu.com>
2020-12-03 15:50:48 +0800

    result collect

--- stock/futu_history_k.py
+++ stock/futu_history_k.py
@@ -3,36 +3,42 @@ import pandas as pd
 from datetime import datetime
 
 
-STOCK_CODE = 'HK.800000'
+STOCK_CODE_MEITUAN = 'HK.03690'
+
+STOCK_CODE_TENGXUN = 'HK.00700'
+
+STOCK_CODES = [STOCK_CODE_MEITUAN, STOCK_CODE_TENGXUN]
+
 START_DATE = '2017-01-01'
 END_DATE = datetime.now().strftime('%Y-%m-%d')
 
-if __name__ == '__main__':
+
+def fetch_stock_datas():
     quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
     ret, data = quote_ctx.get_history_kl_quota(get_detail=True)  # 设置True代表需要返回详细的拉取历史K 线的记录
     if ret == RET_OK:
         print(data)
     else:
         print('error:', data)
-
-    ret, data, page_req_key = quote_ctx.request_history_kline(STOCK_CODE, start=START_DATE, end=END_DATE,
-                                                              max_count=1000)  # 每页5个，请求第一页
-    if ret == RET_OK:
-        print(data)
-        print(data['code'][0])  # 取第一条的股票代码
-        print(data['close'].values.tolist())  # 第一页收盘价转为list
-    else:
-        print('error:', data)
-    data_list = []
-    while page_req_key is not None:  # 请求后面的所有结果
-        print('*************************************')
+    for STOCK_CODE in STOCK_CODES:
         ret, data, page_req_key = quote_ctx.request_history_kline(STOCK_CODE, start=START_DATE, end=END_DATE,
-                                                                  max_count=1000, page_req_key=page_req_key)  # 请求翻页后的数据
+                                                                  max_count=1000)  # 每页5个，请求第一页
         if ret == RET_OK:
-            data_list.append(data)
+            print(data)
+            print(data['code'][0])  # 取第一条的股票代码
+            print(data['close'].values.tolist())  # 第一页收盘价转为list
         else:
             print('error:', data)
-    data_pd = pd.DataFrame(data)
-    data_pd.to_csv('HK800000.csv')
-    print('All pages are finished!')
+        data_list = []
+        while page_req_key is not None:  # 请求后面的所有结果
+            print('*************************************')
+            ret, data, page_req_key = quote_ctx.request_history_kline(STOCK_CODE, start=START_DATE, end=END_DATE,
+                                                                      max_count=1000, page_req_key=page_req_key)  # 请求翻页后的数据
+            if ret == RET_OK:
+                data_list.append(data)
+            else:
+                print('error:', data)
+        data_pd = pd.DataFrame(data)
+        data_pd.to_csv(STOCK_CODE+'.csv')
+        print('All pages are finished!')
     quote_ctx.close()
