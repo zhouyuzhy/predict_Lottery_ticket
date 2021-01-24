d74d46515c07c38e124b710cad8f112373c4a575
zhoushaoyu <zhouyuzhy@126.com>
2021-01-24 23:51:37 +0800

    rsi指标

--- stock/futu_history_k.py
+++ stock/futu_history_k.py
@@ -1,6 +1,8 @@
 from futu import *
 import pandas as pd
 from datetime import datetime
+from pyti.relative_strength_index import relative_strength_index as rsi
+
 
 STOCK_CODE_MEITUAN = 'HK.03690'
 
@@ -27,6 +29,108 @@ STOCK_CODES = [STOCK_CODE_MEITUAN, STOCK_CODE_TENGXUN, STOCK_CODE_BABA, STOCK_CO
 
 START_DATE = '2017-01-01'
 END_DATE = datetime.now().strftime('%Y-%m-%d')
+
+
+def crossesOver(stream1, stream2):
+    # If stream2 is an int or float, check if stream1 has crossed over that fixed number
+    if isinstance(stream2, int) or isinstance(stream2, float):
+        if stream1[len(stream1)-1] <= stream2:
+            return 0
+        else:
+            if stream1[len(stream1)-2] > stream2:
+                return 0
+            elif stream1[len(stream1)-2] < stream2:
+                return 1
+            else:
+                x = 2
+                while stream1[len(stream1)-x] == stream2:
+                    x = x + 1
+                if stream1[len(stream1)-x] < stream2:
+                    return 1
+                else:
+                    return 0
+    # Check if stream1 has crossed over stream2
+    else:
+        if stream1[len(stream1)-1] <= stream2[len(stream2)-1]:
+            return 0
+        else:
+            if stream1[len(stream1)-2] > stream2[len(stream2)-2]:
+                return 0
+            elif stream1[len(stream1)-2] < stream2[len(stream2)-2]:
+                return 1
+            else:
+                x = 2
+                while stream1[len(stream1)-x] == stream2[len(stream2)-x]:
+                    x = x + 1
+                if stream1[len(stream1)-x] < stream2[len(stream2)-x]:
+                    return 1
+                else:
+                    return 0
+
+
+def crossesUnder(stream1, stream2):
+    # If stream2 is an int or float, check if stream1 has crossed under that fixed number
+    if isinstance(stream2, int) or isinstance(stream2, float):
+        if stream1[len(stream1)-1] >= stream2:
+            return 0
+        else:
+            if stream1[len(stream1)-2] < stream2:
+                return 0
+            elif stream1[len(stream1)-2] > stream2:
+                return 1
+            else:
+                x = 2
+                while stream1[len(stream1)-x] == stream2:
+                    x = x + 1
+                if stream1[len(stream1)-x] > stream2:
+                    return 1
+                else:
+                    return 0
+    # Check if stream1 has crossed under stream2
+    else:
+        if stream1[len(stream1)-1] >= stream2[len(stream2)-1]:
+            return 0
+        else:
+            if stream1[len(stream1)-2] < stream2[len(stream2)-2]:
+                return 0
+            elif stream1[len(stream1)-2] > stream2[len(stream2)-2]:
+                return 1
+            else:
+                x = 2
+                while stream1[len(stream1)-x] == stream2[len(stream2)-x]:
+                    x = x + 1
+                if stream1[len(stream1)-x] > stream2[len(stream2)-x]:
+                    return 1
+                else:
+                    return 0
+
+
+def set_rsi(data_pd):
+    data_pd['rsi6'] = pd.Series(rsi(data_pd['close'], 6)).fillna(0).tolist()
+    data_pd['rsi12'] = pd.Series(rsi(data_pd['close'], 12)).fillna(0).tolist()
+    data_pd['rsi24'] = pd.Series(rsi(data_pd['close'], 24)).fillna(0).tolist()
+    data_pd['crossover_6_12'] = [crossesOver(data_pd['rsi6'].values[:i+1], data_pd['rsi12'].values[:i+1]) for i in range(len(data_pd))]
+    data_pd['crossover_6_24'] = [crossesOver(data_pd['rsi6'].values[:i+1], data_pd['rsi24'].values[:i+1]) for i in range(len(data_pd))]
+    data_pd['crossover_12_24'] = [crossesOver(data_pd['rsi12'].values[:i+1], data_pd['rsi24'].values[:i+1]) for i in range(len(data_pd))]
+    upper_rsi = 80
+    lower_rsi = 30
+    data_pd['upper_crossover_rsi_6'] = [crossesOver(data_pd['rsi6'].values[:i+1], upper_rsi) for i in range(len(data_pd))]
+    data_pd['upper_crossover_rsi_12'] = [crossesOver(data_pd['rsi12'].values[:i+1], upper_rsi) for i in range(len(data_pd))]
+    data_pd['upper_crossover_rsi_24'] = [crossesOver(data_pd['rsi24'].values[:i+1], upper_rsi) for i in range(len(data_pd))]
+    data_pd['lower_crossover_rsi_6'] = [crossesOver(data_pd['rsi6'].values[:i+1], lower_rsi) for i in range(len(data_pd))]
+    data_pd['lower_crossover_rsi_12'] = [crossesOver(data_pd['rsi12'].values[:i+1], lower_rsi) for i in range(len(data_pd))]
+    data_pd['lower_crossover_rsi_24'] = [crossesOver(data_pd['rsi24'].values[:i+1], lower_rsi) for i in range(len(data_pd))]
+
+    data_pd['crossunder_6_12'] = [crossesUnder(data_pd['rsi6'].values[:i+1], data_pd['rsi12'].values[:i+1]) for i in range(len(data_pd))]
+    data_pd['crossunder_6_24'] = [crossesUnder(data_pd['rsi6'].values[:i+1], data_pd['rsi24'].values[:i+1]) for i in range(len(data_pd))]
+    data_pd['crossunder_12_24'] = [crossesUnder(data_pd['rsi12'].values[:i+1], data_pd['rsi24'].values[:i+1]) for i in range(len(data_pd))]
+
+    data_pd['lower_crossunder_rsi_6'] = [crossesUnder(data_pd['rsi6'].values[:i+1], lower_rsi) for i in range(len(data_pd))]
+    data_pd['lower_crossunder_rsi_6'] = [crossesUnder(data_pd['rsi6'].values[:i+1], lower_rsi) for i in range(len(data_pd))]
+    data_pd['lower_crossunder_rsi_12'] = [crossesUnder(data_pd['rsi12'].values[:i+1], lower_rsi) for i in range(len(data_pd))]
+    data_pd['upper_crossunder_rsi_6'] = [crossesUnder(data_pd['rsi6'].values[:i+1], upper_rsi) for i in range(len(data_pd))]
+    data_pd['upper_crossunder_rsi_6'] = [crossesUnder(data_pd['rsi6'].values[:i+1], upper_rsi) for i in range(len(data_pd))]
+    data_pd['upper_crossunder_rsi_12'] = [crossesUnder(data_pd['rsi12'].values[:i+1], upper_rsi) for i in range(len(data_pd))]
 
 
 def fetch_stock_datas():
@@ -58,6 +162,11 @@ def fetch_stock_datas():
                 raise Exception('unknown error')
         data_pd = data_list
         data_pd['incr'] = (data_pd['close']-data_pd['last_close']>0).astype(int)
+        set_rsi(data_pd)
         data_pd.to_csv(STOCK_CODE + '.csv')
         print('All pages are finished!')
     quote_ctx.close()
+
+
+if __name__ == '__main__':
+    fetch_stock_datas()
